d441520e4be3a7cf1e73c118c5b31bde670f7796
Igor <pashev.igor@gmail.com>
2010-11-02 17:53:48 +0300

    Added default page name for papers

--- conf/default.php
+++ conf/default.php
@@ -9,3 +9,5 @@ $conf['year_min']    = '2001';
 $conf['papers_ns']   = 'papers';
 $conf['filetypes']   = array('PDF', 'DjVu', 'PS', 'TIFF');
 
+$conf['bibtex']  = 'публикации'; // Wiki page with bibtex data
+
